f632300f904d46f609a27920058476096815bc51
Александр Корниенко <kornsasha89@gmail.com>
2018-07-17 09:53:54 +0300

    Init

--- www/config/db-sample.php
+++ www/config/db-sample.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=acc_sheduler_loc',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
