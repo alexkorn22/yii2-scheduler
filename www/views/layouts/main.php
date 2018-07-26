785fae1d76a499f6d0ee24089aefe4eb366d08e9
alexkorn22 <kornsasha89@gmail.com>
2018-07-26 14:31:09 +0300

    Фильтр медработников

--- www/views/layouts/main.php
+++ www/views/layouts/main.php
@@ -38,15 +38,14 @@ AppAsset::register($this);
         ],
     ]);
     $items = [];
-    $items[] = ['label' => 'Home', 'url' => ['/site/index']];
     $items[] = ['label' => 'Обновить данные', 'url' => \yii\helpers\Url::to(['site/index', 'clear_cache' => 'true'])];
     $items[] = Yii::$app->user->isGuest ? (
-    ['label' => 'Login', 'url' => ['/site/login']]
+    ['label' => 'Войти', 'url' => ['/site/login']]
     ) : (
         '<li>'
         . Html::beginForm(['/site/logout'], 'post')
         . Html::submitButton(
-            'Logout (' . Yii::$app->user->identity->username . ')',
+            'Выйти (' . Yii::$app->user->identity->username . ')',
             ['class' => 'btn btn-link logout']
         )
         . Html::endForm()
